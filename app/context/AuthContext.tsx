c65ab1afa4cca117e109f36927d3784258b4a2ca
phoenix-da-phenom <ehinormichael@ymail.com>
2026-03-21 13:15:34 +0100

    Implemented expo-secure-store

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -1,44 +1,54 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import * as SecureStore from 'expo-secure-store';
 import React, { createContext, ReactNode, useEffect, useState } from "react";
-type AuthContextType ={
-    userToken: string | null;
-    loading: boolean;
-    login: (token:string)=> Promise<void>;
-    logout:()=> Promise<void>;
+type AuthContextType = {
+  userToken: string | null;
+  loading: boolean;
+  login: (token: string) => Promise<void>;
+  logout: () => Promise<void>;
 };
-type Props ={
-    children: ReactNode;
-}
-export const AuthContext = createContext<AuthContextType>({} as AuthContextType);
+type Props = {
+  children: ReactNode;
+};
+export const AuthContext = createContext<AuthContextType>(
+  {} as AuthContextType,
+);
 
-export const AuthProvider =({children}: Props) => {
-    const [userToken, setUserToken]=useState<string | null>(null)
-    const [loading, setLoading]= useState<boolean>(true);
+export const AuthProvider = ({ children }: Props) => {
+  const [userToken, setUserToken] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
-    useEffect(()=>{
-
-        const loadToken = async ()=>{
-            const token = await AsyncStorage.getItem("userToken");
-            setUserToken(token);
-            setLoading(false)
-
-        };
-        loadToken();
-
-
-    },[]);
-    const login = async (token:string)=> {
-        await AsyncStorage.setItem("userToken", token);
+  useEffect(() => {
+    const loadToken = async () => {
+      try {
+        const token = await SecureStore.getItemAsync("userToken");
         setUserToken(token);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    loadToken();
+  }, []);
+  const login = async (token: string) => {
+    try {
+      await SecureStore.setItemAsync("userToken", token);
+      setUserToken(token);
+    } catch (error) {
+      console.error(error);
     }
-    const logout = async ()=>{
-        await AsyncStorage.removeItem("userToken");
-        setUserToken(null)
+  };
+  const logout = async () => {
+    try {
+      await SecureStore.deleteItemAsync('userToken');
+      setUserToken(null);
+    } catch (error) {
+      console.error(error);
     }
-    return(
-        <AuthContext.Provider value ={{ userToken, loading , login , logout }}>
-        {children}
-        
-        </AuthContext.Provider>
-    )
-}
+  };
+  return (
+    <AuthContext.Provider value={{ userToken, loading, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
